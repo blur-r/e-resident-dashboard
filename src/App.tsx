24381d13cffb8d29faf3b8a79e4575c8daff9a02
Babalola Paul <pbabalola27@gmail.com>
2025-09-16 14:26:17 +0100

    create the files for pages, components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-
-
+import './App.css'
 function App() {
 
     return (
         <>
-
+            <div className="text-amber-500 text-3xl">
+                hello World
+            </div>
         </>
     )
 }
